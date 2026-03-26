a66af9802e18cc216a22d96f6b9ef9e99894cb02
Mikulski-Ivan <Ivan.S.Mikulski@gmail.com>
2026-03-26 15:57:14 +0300

    rename envs for host and port

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "Starting FastAPI server..."
 exec uvicorn main:app \
-    --host "${HOST:-0.0.0.0}" \
-    --port "${PORT:-8000}" \
+    --host "${LLM_HOST:-0.0.0.0}" \
+    --port "${LLM_PORT:-8000}" \
